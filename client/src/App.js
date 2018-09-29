743aad6211fcb0c0028ece5332059e660f61b169
Mohit <mhtkmr303@gmail.com>
2018-09-29 20:11:35 +0530

    login success

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import jwt_decode from "jwt -decode";
+import setAuthToken from "./utils/setAuthToken";
+import { setCurrentUser } from "./actions/authActions";
 import { Provider } from "react-redux";
 import store from "./store";
 
@@ -10,6 +13,17 @@ import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
 
 import "./App.css";
+
+//check for token
+
+if (localStorage.jwtToken) {
+  //set token
+  setAuthToken(localStorage.jwtToken);
+  //decode token
+  const decoded = jwt_decode(localStorage.jwtToken);
+  //set user and isAuthenticated
+  store.dispatch(setCurrentUser(decoded));
+}
 
 class App extends Component {
   render() {
