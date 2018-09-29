81b906f760a8c33436233d54cc34f54244d15c36
Mohit <mhtkmr303@gmail.com>
2018-09-29 17:18:09 +0530

    redux

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,5 +1,7 @@
 import axios from "axios";
-import { GET_ERRORS } from "./types";
+import setAuthToken from "../utils/setAuthToken";
+import { GET_ERRORS, SET_CURRENT_USER } from "./types";
+import jwt_decode from "jwt-decode";
 
 //Register user
 export const registerUser = (userData, history) => dispatch => {
@@ -12,4 +14,31 @@ export const registerUser = (userData, history) => dispatch => {
         payload: err.response.data
       })
     );
+};
+
+//Login - Get_TOken
+
+export const loginUser = userData => dispatch => {
+  axios
+    .post("/api/users/login", userData)
+    .then(res => {
+      const { token } = res.data;
+      localStorage.setItem("jwtToken", token);
+      setAuthToken(token);
+      const decoded = jwt_decode(token);
+      dispatch(setCurrentUser(decoded));
+    })
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
+export const setCurrentUser = decoded => {
+  return {
+    type: SET_CURRENT_USER,
+    payload: decoded
+  };
 };
